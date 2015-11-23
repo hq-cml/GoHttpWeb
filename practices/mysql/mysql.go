c048234b5e2d0fde0b0dcc36534e3d164917a4eb
hq <hq_cml@163.com>
2015-11-24 07:38:15 +0800

    create table sql

--- practices/mysql/mysql.go
+++ practices/mysql/mysql.go
@@ -1,8 +1,27 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 )
+
+/*
+CREATE TABLE `userinfo` (
+    `uid` INT(10) NOT NULL AUTO_INCREMENT,
+    `username` VARCHAR(64) NULL DEFAULT NULL,
+    `departname` VARCHAR(64) NULL DEFAULT NULL,
+    `created` DATE NULL DEFAULT NULL,
+    PRIMARY KEY (`uid`)
+)
+
+CREATE TABLE `userdetail` (
+    `uid` INT(10) NOT NULL DEFAULT '0',
+    `intro` TEXT NULL,
+    `profile` TEXT NULL,
+    PRIMARY KEY (`uid`)
+)
+*/
 
 func main() {
 	fmt.Println("mysql")
