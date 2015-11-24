bb6ccd7c35c199efc92410d1b22d29406e7ba0ca
hq <hq_cml@163.com>
2015-11-24 08:37:01 +0800

    get

--- practices/redis/redis.go
+++ practices/redis/redis.go
@@ -9,4 +9,10 @@ func main() {
 	var client goredis.Client
 	// 设置端口为redis默认端口
 	client.Addr = "127.0.0.1:6379"
+
+	//字符串操作
+	client.Set("a", []byte("hello"))
+	val, _ := client.Get("a")
+	fmt.Println(string(val))
+	client.Del("a")
 }
