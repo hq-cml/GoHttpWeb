e1d82c1798ef9e2e168394e45669ab4aa64f1ac7
hq <hq_cml@163.com>
2015-11-25 08:09:09 +0800

    session

---
+++ practices/session/session.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+//目前Go标准包没有为session提供任何支持，所以，自己实现
+func sayhelloName(w http.ResponseWriter, r *http.Request) {
+	//设置cookie
+	expiration := time.Now()
+	expiration = expiration.AddDate(1, 0, 0)
+	cookie := http.Cookie{Name: "username", Value: "hq", Expires: expiration}
+	http.SetCookie(w, &cookie)
+
+	//读取cookie
+	c, _ := r.Cookie("username")
+
+	fmt.Fprintf(w, "Hello world~. Your cookie is:%s:%s", c.Name, c.Value) //这个写入到w的是输出到客户端的
+}
+
+func main() {
+	http.HandleFunc("/", sayhelloName)       //设置访问的路由
+	err := http.ListenAndServe(":9527", nil) //设置监听的端口
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
