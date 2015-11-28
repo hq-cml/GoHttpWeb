d3c93817da6078eb1e8f6fcc4fc36ea07516e979
hq <hq_cml@163.com>
2015-11-28 18:23:18 +0800

    some commont

--- practices/session/session/session.go
+++ practices/session/session/session.go
@@ -55,26 +55,26 @@ type Storage interface {
 
 //session管理器
 type SessionManager struct {
-	cookieName  string     //private cookiename，？？做什么用
-	lock        sync.Mutex //protects session
-	storager    Storage    //一种具体的存储实现
-	maxlifetime int64      //最大有效期，用于GC
-}
-
-//创建管理器
-func NewManager(storageName, cookieName string, maxlifetime int64) (*SessionManager, error) {
-	storager, ok := g_storages[storageName]
-	if !ok {
-		return nil, fmt.Errorf("session: unknown storage %q (forgotten import?)", storageName)
-	}
-	return &SessionManager{storager: storager, cookieName: cookieName, maxlifetime: maxlifetime}, nil
+	cookie_name   string     //cookie的名字（SessionId以cookie形式传到客户端）
+	lock          sync.Mutex //protects session
+	storager      Storage    //一种具体的存储实现
+	max_life_time int64      //最大有效期，用于GC
 }
 
 /*
  * 参照database/sql/driver，先定义好接口，然后具体的存储session的结构只需要：1.实现相应的接口；2.注册，
- * 相应功能这样就可以使用了，storages全局变量负责存储全部的storager实现，Register函数负责填充storages
+ * 相应功能这样就可以使用了，g_storages全局变量负责存储全部的storage实现，Register函数负责填充g_storages
  */
 var g_storages = make(map[string]Storage)
+
+//创建管理器
+func NewManager(storage_name, cookie_name string, max_life_time int64) (*SessionManager, error) {
+	storager, ok := g_storages[storage_name]
+	if !ok {
+		return nil, fmt.Errorf("session: unknown storage %q (forgotten import?)", storage_name)
+	}
+	return &SessionManager{storager: storager, cookie_name: cookie_name, max_life_time: max_life_time}, nil
+}
 
 /*
  * Register函数，注册一个可用的storager。不能重复注册
@@ -99,26 +99,30 @@ func (manager *SessionManager) sessionId() string {
 	return base64.URLEncoding.EncodeToString(b)
 }
 
-//SessionStart函数，检测是否已经有某个Session与当前来访用户发生了关联，如果没有则创建之。
+//SessionStart函数：
+//检查用户request的cookie中对应sid的值，如果没有，则创建sid；如果有，则读取session值，这个值又是什么呢？？？
 func (manager *SessionManager) SessionStart(w http.ResponseWriter, r *http.Request) (session Session) {
 	manager.lock.Lock()
 	defer manager.lock.Unlock()
-	cookie, err := r.Cookie(manager.cookieName)
+	cookie, err := r.Cookie(manager.cookie_name)
 	if err != nil || cookie.Value == "" {
-		sid := manager.sessionId()
+		sid := manager.sessionId() //生成全局唯一的sid
 		session, _ = manager.storager.SessionInit(sid)
-		cookie := http.Cookie{Name: manager.cookieName, Value: url.QueryEscape(sid), Path: "/", HttpOnly: true, MaxAge: int(manager.maxlifetime)}
+		cookie := http.Cookie{Name: manager.cookie_name, Value: url.QueryEscape(sid), Path: "/", HttpOnly: true, MaxAge: int(manager.max_life_time)}
 		http.SetCookie(w, &cookie)
 	} else {
+		//cookie[cookie_name]对应的值，其实是sessionid!
 		sid, _ := url.QueryUnescape(cookie.Value)
 		session, _ = manager.storager.SessionRead(sid)
 	}
 	return
 }
 
-//Destroy sessionid
+//Destroy session
+//1. 服务端：先调用对应storager的sessiondestroy函数
+//2. 客户端：然后让客户端清除cookie
 func (manager *SessionManager) SessionDestroy(w http.ResponseWriter, r *http.Request) {
-	cookie, err := r.Cookie(manager.cookieName)
+	cookie, err := r.Cookie(manager.cookie_name)
 	if err != nil || cookie.Value == "" {
 		return
 	} else {
@@ -133,11 +137,11 @@ func (manager *SessionManager) SessionDestroy(w http.ResponseWriter, r *http.Req
 	}
 }
 
-//GC
 //利用了time包中的定时器功能，当超时maxLifeTime之后调用GC函数，这样就可以保证maxLifeTime时间内的session是可用的
+//首先调用对应storager的GC，然后启动一个定时器，触发自己，也就是说manager的GC会定期触发~
 func (manager *SessionManager) GC() {
 	manager.lock.Lock()
 	defer manager.lock.Unlock()
 	manager.storager.SessionGC(manager.maxlifetime)
-	time.AfterFunc(time.Duration(manager.maxlifetime), func() { manager.GC() })
+	time.AfterFunc(time.Duration(manager.maxlifetime), func() { manager.GC() }) //自己调用自己
 }
