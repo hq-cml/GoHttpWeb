3b6a1a7b6575ca6ec040d67825ca3db9651bd5bf
huaqi <huaqi@didichuxing.com>
2015-11-30 20:58:49 +0800

    add set

--- practices/session/session/storages/redis.go
+++ practices/session/session/storages/redis.go
@@ -14,9 +14,9 @@ import (
  * 这是一个用户对应的session结构，而不是整体的session结构
  */
 type RedisSession struct {
-	sid           string                      //session id唯一标示
-	time_accessed time.Time                   //最后访问时间
-	value         map[interface{}]interface{} //session里面存储的值
+	sid           string    //session id唯一标示
+	time_accessed time.Time //最后访问时间
+	//value         map[interface{}]interface{} //session里面存储的值
 }
 
 /*
@@ -24,16 +24,29 @@ type RedisSession struct {
  * 这是一个整体session的对应的结构
  */
 type RedisStorage struct {
-	lock     sync.Mutex               //锁
-	sessions map[string]*list.Element //用于存储的内存，key是sid，value是list的Element（其实本质上，是一个）
-	list     *list.List               //链表，用于gc
+	redis_client goredis.Client
+	lock         sync.Mutex //锁
+	//sessions map[string]*list.Element //用于存储的内存，key是sid，value是list的Element（其实本质上，是一个）
+	list *list.List //链表，用于gc
 }
 
 var g_redis_storage = &RedisStorage{}
 
 func init() {
 	fmt.Println("Redis storage init")
+	// 设置端口为redis默认端口
+	g_redis_storage.redis_client.Addr = "127.0.0.1:6379"
 	g_redis_storage.list = list.New()
-	g_redis_storage.sessions = make(map[string]*list.Element, 0)
+	//g_redis_storage.sessions = make(map[string]*list.Element, 0)
 	session.Register("redis", g_redis_storage)
+}
+
+/*
+ * RedisSession实现Session接口的：Set/Get/Delete/SessionID方法
+ */
+func (self *RedisSession) Set(key, value interface{}) error {
+	self.redis_client.Hset(self.sid, key, []byte(value))
+	//更新对应条目的访问时间
+	g_memstorage.SessionUpdate(self.sid)
+	return nil
 }
